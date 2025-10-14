a17cc70096832cb36c586e029f688942f95b53b8
RudranathKabiraj <kabirajrudranath@gmail.com>
2025-10-14 20:51:06 +0530

    ready for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,16 +1,30 @@
 import express from "express";
-import dotenv from "dotenv";
+    import dotenv from "dotenv";
+import path from "path";
 
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js"
 
-dotenv.config();
+    dotenv.config();
 
 const app = express();
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
+const __dirname = path.resolve();
+
 
 app.use("/api/auth",authRoutes)
 app.use("/api/message" , messageRoutes)
 
-app.listen(PORT, () => console.log("server is running at port "+PORT));
+//make ready for deployment
+if(process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")))   //express.static() is used to serve the frontend build files (HTML, CSS, JS) directly from the backend.
+
+
+    //For any route not matching an API, send index.html so the React app can handle routing.
+    app.get("*", (_,res) => {
+        res.sendFile(path.join(__dirname, "../frontend/dist/index.html"))
+    });
+
+}
+app.listen(PORT, () => console.log("server is running at port: "+PORT));
